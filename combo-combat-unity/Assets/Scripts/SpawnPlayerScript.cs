3d66197d668e479693fcba0dd399af33f53da3db
Manuel Araoz <manuelaraoz@gmail.com>
2012-11-13 03:31:48 -0200

    first unity version
    marse puto

---
+++ combo-combat-unity/Assets/Scripts/SpawnPlayerScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpawnPlayerScript : MonoBehaviour {
+
+    public Transform playerPrefab;
+
+    void Start() {
+        Network.isMessageQueueRunning = true;
+        Network.SetSendingEnabled(0, true);
+        if (Network.isClient)
+            Network.Instantiate(playerPrefab, transform.position, Quaternion.identity, 0);
+    }
+
+    void OnPlayerDisconnected (NetworkPlayer player)
+    {
+	    Debug.Log("Server destroying player");
+	    Network.RemoveRPCs(player, 0);
+	    Network.DestroyPlayerObjects(player);
+    }
+
+    void OnGUI() {
+        if (Network.isServer)
+            GUI.Label(new Rect(20, Screen.height - 50, 200, 20), "Running as a dedicated server");
+    }
+
+}
