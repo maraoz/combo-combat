a3f108f58b7f23508620827db70e4c1fa3899378
Manuel Araoz <manuelaraoz@gmail.com>
2012-11-14 17:23:34 -0200

    migrating to C# more scripts

--- combo-combat-unity/Assets/Scripts/SpawnPlayerScript.cs
+++ combo-combat-unity/Assets/Scripts/SpawnPlayerScript.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class SpawnPlayerScript : MonoBehaviour {
 
-    public Transform playerPrefab;
+	public Transform playerPrefab;
 
-    void Start() {
-        Network.isMessageQueueRunning = true;
-        Network.SetSendingEnabled(0, true);
-        if (Network.isClient)
-            Network.Instantiate(playerPrefab, transform.position, Quaternion.identity, 0);
-    }
+	void Start() {
+		Network.isMessageQueueRunning = true;
+		Network.SetSendingEnabled(0, true);
+		if (Network.isClient)
+			Network.Instantiate(playerPrefab, transform.position, Quaternion.identity, 0);
+	}
 
-    void OnPlayerDisconnected (NetworkPlayer player)
-    {
-	    Debug.Log("Server destroying player");
-	    Network.RemoveRPCs(player, 0);
-	    Network.DestroyPlayerObjects(player);
-    }
+	void OnPlayerDisconnected (NetworkPlayer player)
+	{
+		Debug.Log("Server destroying player");
+		Network.RemoveRPCs(player, 0);
+		Network.DestroyPlayerObjects(player);
+	}
 
-    void OnGUI() {
-        if (Network.isServer)
-            GUI.Label(new Rect(20, Screen.height - 50, 200, 20), "Running as a dedicated server");
-    }
+	void OnGUI() {
+		if (Network.isServer)
+			GUI.Label(new Rect(20, Screen.height - 50, 200, 20), "Running as a dedicated server");
+	}
 
 }
