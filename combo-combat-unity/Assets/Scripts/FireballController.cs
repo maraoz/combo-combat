f9c7259256a456b63aa36ea19e9f04921ed37272
Manuel Araoz <manuelaraoz@gmail.com>
2012-11-15 20:03:08 -0200

    fireball assets :D

---
+++ combo-combat-unity/Assets/Scripts/FireballController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class FireballController : MonoBehaviour {
+
+    private float speed = .1f;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.position += transform.TransformDirection(Vector3.forward) * speed;
+    }
+}
---
+++ combo-combat-unity/Assets/Scripts/Test/FireballRain.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class FireballRain : MonoBehaviour {
+
+    public GameObject fireballPrefab;
+    public float timeBetweenSpawn = 1.0f;
+    public float range = 10;
+
+    private float accumTime;
+
+    // Use this for initialization
+    void Start () {
+        accumTime = 0.0f;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        accumTime += Time.deltaTime;
+        if (accumTime >= timeBetweenSpawn) {
+            accumTime -= timeBetweenSpawn;
+            GameObject.Instantiate(fireballPrefab,
+                new Vector3(transform.position.x + Random.Range(-range, range),
+                    transform.position.y,
+                    transform.position.z + Random.Range(-range, range)),
+                Quaternion.Euler(90.0f,0.0f,0.0f));
+        }
+    }
+}
