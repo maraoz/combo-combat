fd544a346c683a79347c44dd2b10afe1ee969b42
Manuel Araoz <manuelaraoz@gmail.com>
2012-11-15 23:22:42 -0200

    explosion para fireball

--- combo-combat-unity/Assets/Scripts/FireballController.cs
+++ combo-combat-unity/Assets/Scripts/FireballController.cs
@@ -3,20 +3,29 @@ using System.Collections;
 
 public class FireballController : MonoBehaviour {
 
+    public GameObject explosion;
+    public float secondsUntilExhaust = 3.0f;
+    public float secondsPast;
+    
     private float speed = .2f;
 
     // Use this for initialization
     void Start () {
-    
+        secondsPast = 0.0f;
     }
     
     // Update is called once per frame
     void Update () {
         transform.position += transform.TransformDirection(Vector3.forward) * speed;
+        secondsPast += Time.deltaTime;
+        if (secondsPast >= secondsUntilExhaust) {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         Destroy(gameObject);
+        GameObject.Instantiate(explosion, transform.position, Quaternion.identity);
     }
 
     public void SetSpeed(float s) {
