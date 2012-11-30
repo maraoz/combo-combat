05e5360eccf36a0bdf06a5e0cd855ccac33fc56d
Manuel Araoz <manuelaraoz@gmail.com>
2012-11-30 21:07:13 -0200

    intentando arreglar el bug del heart collider effect

--- combo-combat-unity/Assets/Scripts/Characters/MageHeartCollide.cs
+++ combo-combat-unity/Assets/Scripts/Characters/MageHeartCollide.cs
@@ -5,18 +5,8 @@ public class MageHeartCollide : MonoBehaviour {
 
     public GameObject effect;
 
-    void Awake() {
-        if (!networkView.isMine) {
-            Debug.Log("not mine colliding!");
-        }
-    }
-
     void OnControllerColliderHit(ControllerColliderHit hit) {
-        if (!networkView.isMine) {
-            Debug.Log("not mine colliding!");
-        }
         if (hit.collider.tag == GameConstants.HEART_TAG) {
-            Debug.Log("On collider hit heart");
             if (effect != null) {
                 GameObject instance = GameObject.Instantiate(effect, transform.position, Quaternion.identity) as GameObject;
                 instance.transform.parent = transform;
