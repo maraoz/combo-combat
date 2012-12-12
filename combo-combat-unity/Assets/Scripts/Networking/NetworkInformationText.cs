68ae09534c85e5c4d1f27156677fb19349ac5074
Manuel Araoz <manuelaraoz@gmail.com>
2012-12-11 22:07:32 -0300

    version text rect extended

--- combo-combat-unity/Assets/Scripts/Networking/NetworkInformationText.cs
+++ combo-combat-unity/Assets/Scripts/Networking/NetworkInformationText.cs
@@ -9,9 +9,9 @@ public class NetworkInformationText : MonoBehaviour {
 
     void OnGUI() {
         if (Network.isServer)
-            GUI.Label(new Rect(20, Screen.height - 20, 200, 20), "Running as a dedicated server");
+            GUI.Label(new Rect(20, Screen.height - 20, 500, 20), "Running as a dedicated server");
         if (Network.isClient) {
-            GUI.Label(new Rect(20, Screen.height - 20, 200, 20), "Combo Combat version " + GameConstants.gameVersion);
+            GUI.Label(new Rect(20, Screen.height - 20, 500, 20), "Combo Combat version " + GameConstants.gameVersion);
         }
     }
 }
