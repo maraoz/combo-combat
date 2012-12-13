06bd1e87712486d146bd3e8bb98ff1c2541f9cfd
Manuel Araoz <manuelaraoz@gmail.com>
2012-12-13 20:51:44 -0300

    new fucking awesome wall effect

--- combo-combat-unity/Assets/Scripts/Spells/WallController.cs
+++ combo-combat-unity/Assets/Scripts/Spells/WallController.cs
@@ -7,11 +7,15 @@ public class WallController : MonoBehaviour {
     public float duration = 3;
     private float timePassed;
 
-    void Start () {
+    void Start() {
         timePassed = 0;
     }
-    
-    void Update () {
+
+    void Update() {
+        // Animates main texture scale in a funky way!
+        float scaleX = Mathf.Cos(Time.time) * 0.5f + 1;
+        float scaleY = Mathf.Sin(Time.time) * 0.5f + 1;
+        renderer.material.mainTextureScale = new Vector2(scaleX, scaleY);
         timePassed += Time.deltaTime;
         if (timePassed >= duration) {
             Destroy(gameObject);
