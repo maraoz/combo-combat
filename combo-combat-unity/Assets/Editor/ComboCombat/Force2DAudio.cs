f85951a033819aced341e13007550e2b1d4f22c7
Manuel Araoz <manuelaraoz@gmail.com>
2013-01-15 19:07:13 -0300

    editor audio script

---
+++ combo-combat-unity/Assets/Editor/ComboCombat/Force2DAudio.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEditor;
+
+public class Force2dAudio : AssetPostprocessor {
+    void OnPreprocessAudio() {
+        AudioImporter importer = (AudioImporter) assetImporter;
+        importer.threeD = false;
+    }
+}
