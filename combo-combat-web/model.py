b4efdb8a8ae7c5f2b3822d12e57602a5f24d364e
Manuel Araoz <manuelaraoz@gmail.com>
2013-05-06 17:25:20 -0300

    modelling match history

--- combo-combat-web/model.py
+++ combo-combat-web/model.py
@@ -8,3 +8,14 @@ class Player(db.Model):
     password = db.StringProperty()
     
     joined = db.DateTimeProperty(auto_now_add=True)
+
+
+class MatchHistory(db.Model):
+    """Models a match played"""
+    players = db.ListProperty(db.ReferenceProperty(Player, collection_name='matches'))
+    scores = db.ListProperty(db.IntegerProperty)
+    elo_delta = db.IntegerProperty()
+    duration = db.IntegerProperty()
+    
+    played = db.DateTimeProperty(auto_now_add=True)
+    
