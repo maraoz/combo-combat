b464243a0af1f98899d9dc447a492ac5f2fbe497
Manuel Araoz <manuelaraoz@gmail.com>
2013-05-09 19:22:35 -0300

    first API version

--- combo-combat-web/main.py
+++ combo-combat-web/main.py
@@ -11,7 +11,7 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
-def hash(x):
+def hash_digest(x):
     hasher = hashlib.new('ripemd160')
     hasher.update(x)
     return hasher.hexdigest()
@@ -35,16 +35,16 @@ class RegisterHandler(JsonAPIHandler):
         password = self.request.get("p")
         
         if not username or not password:
-            return {"registered": False , "error": "format"}
+            return {"success": False , "error": "format"}
         
         same_name = Player.all().filter('username =', username)
         if same_name.get():
-            return {"registered": False , "error": "username"}
+            return {"success": False , "error": "username"}
         
         
-        player = Player(username=username, password=hash(password))
+        player = Player(username=username, password=hash_digest(password))
         player.put()
-        return {"registered": True}
+        return {"success": True}
 
 class LoginHandler(JsonAPIHandler):
     def handle(self):
@@ -52,18 +52,73 @@ class LoginHandler(JsonAPIHandler):
         password = self.request.get("p")
         
         if not username or not password:
-            return {"login": False , "error": "format"}
+            return {"success": False , "error": "format"}
         
-        player = Player.all().filter('username =', username).filter('password', hash(password)).get()
+        player = Player.all().filter('username =', username).filter('password', hash_digest(password)).get()
         if not player:
-            return {"login": False , "error": "failed"}
-        return {"login": True}
+            return {"success": False , "error": "failed"}
+        return {"success": True}
         
+class SearchHandler(JsonAPIHandler):
+    def handle(self):
+        username = self.request.get("u")
+        if not username:
+            return {"success": False, "error": "format"}
+        player = Player.all().filter('username = ', username).get()
+        if not player:
+            return {"success": False, "error": "nonexisting"}
         
+        other = Player.all().filter('searching = ', True).get()
+        if other:
+            
+            server = Server.get_free()
+            
+            other.searching = False
+            player.searching = False
+            other.match_server = server
+            player.match_server = server
+            server.free = False
+            return {}
+        
+        player.searching = True
+        return {"success": True}
+
+class CheckHandler(JsonAPIHandler):
+    def handle(self):
+        username = self.request.get("u")
+        if not username:
+            return {"success": False, "error": "format"}
+
+        player = Player.all().filter('username = ', username).get()
+        if not player:
+            return {"success": False, "error": "nonexisting"}
+        
+        if not player.match_server:
+            return {"success": True, "host": None}
+        
+        return {"success": True, "host": player.match_server.host}
+    
+class ResetHandler(JsonAPIHandler):
+    def handle(self):
+        host = self.request.get("h")
+        if not host:
+            return {"success": False, "error": "format"}
+
+        server = Server.all().filter('host = ', host).get()
+        if not server:
+            return {"success": False, "error": "nonexisting"}
+        server.free = True
+        return {"success": True}
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/api/register', RegisterHandler),
-    ('/api/login', LoginHandler)
+    ('/api/login', LoginHandler),
+    ('/api/search', SearchHandler),
+    ('/api/check', CheckHandler),
+    ('/api/reset', ResetHandler)
+    
 ], debug=False)
 
--- combo-combat-web/model.py
+++ combo-combat-web/model.py
@@ -1,6 +1,14 @@
 
 from google.appengine.ext import db
 
+class Server(db.Model):
+    """Models a game server that can host a match"""
+    host = db.StringProperty()
+    free = db.BooleanProperty()
+    
+    @classmethod
+    def get_free(cls):
+        return cls.all().filter("free =", True).get()
 
 class Player(db.Model):
     """Models an individual Player, with username, email, and password"""
@@ -8,6 +16,10 @@ class Player(db.Model):
     password = db.StringProperty()
     
     joined = db.DateTimeProperty(auto_now_add=True)
+    
+    # TODO: take this out of here?
+    searching = db.BooleanProperty()
+    match_server = db.ReferenceProperty(Server, collection_name='players')
 
 
 class MatchHistory(db.Model):
