ebc32fa3dae024c6bfbc31a5a7842a0046cf4794
Manuel Araoz <manuelaraoz@gmail.com>
2013-06-13 19:38:21 -0300

    matchmaking

--- combo-combat-unity/Assets/Scripts/GUI/LobbyGUI.cs
+++ combo-combat-unity/Assets/Scripts/GUI/LobbyGUI.cs
@@ -6,61 +6,17 @@ using Boomlagoon.JSON;
 public class LobbyGUI : MonoBehaviour {
 
 
-    public string gameName = "You must change this";
-    public bool allowsDedicatedServer = false;
+    public bool allowsLocalConnection = true;
     public GUISkin customSkin;
 
-    private ConnectionTesterStatus connectionTestResult = ConnectionTesterStatus.Undetermined;
-    private bool filterNATHosts = false;
-    private bool probingPublicIP = false;
-    private bool doneTesting = false;
-    private double timer = 0.0;
-    private bool useNat = true;
 
     private Rect windowRect;
     private Rect serverListRect;
     private Rect creditsRect;
-    private string testMessage = "Undetermined NAT capabilities";
     private string message = "";
     private int attempts;
     private bool waitingForOpponent = false;
 
-
-    void Awake() {
-        connectionTestResult = Network.TestConnection();
-    }
-
-    void Start() {
-        if (allowsDedicatedServer && CommandLineParser.IsBatchMode()) {
-            DoStartServer();
-        }
-    }
-
-    void DoStartServer() {
-
-        NetworkConnectionError error = NetworkConnectionError.CreateSocketOrThreadFailure;
-        int port = CommandLineParser.GetServerPort();
-        while (error != NetworkConnectionError.NoError) {
-            error = Network.InitializeServer(CommandLineParser.GetMaxPlayersAllowed(), port, useNat);
-            if (CommandLineParser.IsDynamicPort()) {
-                port += 1;
-            } else {
-                if (error != NetworkConnectionError.NoError)
-                    Debug.LogError(error);
-                break;
-            }
-            if (port > CommandLineParser.GetServerPort() + 100) {
-                break; // timeout
-            }
-        }
-        MasterServer.RegisterHost(gameName, CommandLineParser.GetServerName(), CommandLineParser.GetServerComment());
-    }
-
-    void Update() {
-        // If test is undetermined, keep running
-        if (!doneTesting)
-            TestConnection();
-    }
 
     void OnGUI() {
         GUI.skin = customSkin;
@@ -88,32 +44,29 @@ public class LobbyGUI : MonoBehaviour {
             GUILayout.BeginHorizontal();
             GUILayout.Space(10);
             // Start a new server
-            if (allowsDedicatedServer) {
-                if (GUILayout.Button("Start Server")) {
-                    DoStartServer();
-                }
+            if (allowsLocalConnection) {
                 if (GUILayout.Button("Connect locally")) {
                     Network.Connect("localhost", CommandLineParser.GetServerPort());
                 }
-            }
 
-            GUILayout.FlexibleSpace();
-            GUILayout.Label("Playing as " + UsernameHolder.GetUsername());
-            if (GUILayout.Button("Logout")) {
-                Application.Quit();
-            }
-
-            GUILayout.EndHorizontal();
-            GUILayout.Space(50);
-            if (GUILayout.Button("Quick Match")) {
-                StartCoroutine(OnQuickPressed());
-            }
-            GUILayout.Box(message, GUILayout.Height(100f));
-            if (waitingForOpponent == true) {
-                if (GUILayout.Button("Cancel search")) {
-                    waitingForOpponent = false;
-                    attempts = 0;
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Playing as " + UsernameHolder.GetUsername());
+                if (GUILayout.Button("Logout")) {
+                    Application.Quit();
                 }
+
+                GUILayout.EndHorizontal();
+                GUILayout.Space(50);
+                if (!waitingForOpponent && GUILayout.Button("Quick Match")) {
+                    StartCoroutine(OnQuickPressed());
+                }
+                if (waitingForOpponent == true) {
+                    if (GUILayout.Button("Cancel search")) {
+                        waitingForOpponent = false;
+                        attempts = 0;
+                    }
+                }
+                GUILayout.Box(message, GUILayout.Height(100f));
             }
         }
     }
@@ -122,6 +75,7 @@ public class LobbyGUI : MonoBehaviour {
         if (waitingForOpponent) {
             yield return null;
         }
+        waitingForOpponent = true;
         string username = UsernameHolder.GetUsername();
         WWW www = new WWW("http://www.combocombat.com/api/search?u=" + username);
         message = "Contacting matchmaking server...";
@@ -135,7 +89,6 @@ public class LobbyGUI : MonoBehaviour {
         bool success = json.GetBoolean("success");
         if (success == true) {
             message = "Waiting for opponent...";
-            waitingForOpponent = true;
             StartCoroutine(OnLookOpponent());
         } else {
             message = "Quick match failed: " + json.GetString("error");
@@ -165,7 +118,8 @@ public class LobbyGUI : MonoBehaviour {
                     message = "Waiting cancelled.";
                 }
             } else {
-                message = "Opponent found! Please connect to " + host;
+                message = "Opponent found! Connecting to " + host;
+                ConnectToServer(host);
                 attempts = 0;
             }
         } else {
@@ -173,38 +127,13 @@ public class LobbyGUI : MonoBehaviour {
         }
     }
 
-    void MakeClientWindow(int id) {
-        GUILayout.Space(25);
+    void ConnectToServer(string host_ip) {
+        String[] split = host_ip.Split(':');
+        String ip = split[0];
+        int port = int.Parse(split[1]);
 
-        HostData[] data = MasterServer.PollHostList();
-        //data = NoInternetVersion();
-        foreach (HostData element in data) {
-            GUILayout.Space(10);
-            GUILayout.BeginHorizontal();
-
-
-            // Do not display NAT enabled games if we cannot do NAT punchthrough
-            if (!(filterNATHosts && element.useNat)) {
-                var connections = "(" + (element.connectedPlayers - 1) + "/" + (element.playerLimit - 1) + ")";
-                GUILayout.Label(element.gameName);
-                GUILayout.Space(5);
-                GUILayout.Label(connections);
-                GUILayout.Space(5);
-                GUILayout.Label(element.comment);
-                GUILayout.Space(5);
-                GUILayout.FlexibleSpace();
-                GUILayout.Width(100);
-                if (element.connectedPlayers < element.playerLimit) {
-                    if (GUILayout.Button("Join Match", "ShortButton")) {
-                        Network.Connect(element);
-                        this.enabled = false;
-                    }
-                } else {
-                    GUILayout.Label("Match has already started");
-                }
-            }
-            GUILayout.EndHorizontal();
-        }
+        Network.Connect(ip, port);
+        this.enabled = false;
     }
 
     private HostData[] NoInternetVersion() {
@@ -327,82 +256,8 @@ public class LobbyGUI : MonoBehaviour {
 
     }
 
-    void TestConnection() {
-        // Start/Poll the connection test, report the results in a label and react to the results accordingly
-        connectionTestResult = Network.TestConnection();
-        switch (connectionTestResult) {
-            case ConnectionTesterStatus.Error:
-                testMessage = "Problem determining NAT capabilities";
-                doneTesting = true;
-                break;
-
-            case ConnectionTesterStatus.Undetermined:
-                testMessage = "Undetermined NAT capabilities";
-                doneTesting = false;
-                break;
-
-            case ConnectionTesterStatus.PublicIPIsConnectable:
-                testMessage = "Directly connectable public IP address.";
-                useNat = false;
-                doneTesting = true;
-                break;
-
-            // This case is a bit special as we now need to check if we can 
-            // circumvent the blocking by using NAT punchthrough
-            case ConnectionTesterStatus.PublicIPPortBlocked:
-                testMessage = "Non-connectble public IP address (port " + CommandLineParser.GetServerPort() + " blocked), running a server is impossible.";
-                useNat = false;
-                // If no NAT punchthrough test has been performed on this public IP, force a test
-                if (!probingPublicIP) {
-                    connectionTestResult = Network.TestConnectionNAT();
-                    probingPublicIP = true;
-                    timer = Time.time + 10;
-                }
-                    // NAT punchthrough test was performed but we still get blocked
-                else if (Time.time > timer) {
-                    probingPublicIP = false; 		// reset
-                    useNat = true;
-                    doneTesting = true;
-                }
-                break;
-            case ConnectionTesterStatus.PublicIPNoServerStarted:
-                testMessage = "Public IP address but server not initialized, it must be started to check server accessibility. Restart connection test when ready.";
-                break;
-
-            case ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted:
-                testMessage = "Limited NAT punchthrough capabilities. Cannot connect to all types of NAT servers.";
-                useNat = true;
-                doneTesting = true;
-                break;
-
-            case ConnectionTesterStatus.LimitedNATPunchthroughSymmetric:
-                testMessage = "Limited NAT punchthrough capabilities. Cannot connect to all types of NAT servers. Running a server is ill adviced as not everyone can connect.";
-                useNat = true;
-                doneTesting = true;
-                break;
-
-            case ConnectionTesterStatus.NATpunchthroughAddressRestrictedCone:
-            case ConnectionTesterStatus.NATpunchthroughFullCone:
-                testMessage = "NAT punchthrough capable. Can connect to all servers and receive connections from all clients. Enabling NAT punchthrough functionality.";
-                useNat = true;
-                doneTesting = true;
-                break;
-
-            default:
-                testMessage = "Error in test routine, got " + connectionTestResult;
-                break;
-        }
-        if (!testMessage.Contains("Undetermined")) {
-            //Debug.Log(testMessage);
-        }
-    }
-
 
     // network events
-    void OnFailedToConnectToMasterServer(NetworkConnectionError info) {
-        Debug.Log(info);
-    }
-
     void OnFailedToConnect(NetworkConnectionError info) {
         Debug.Log(info);
         this.enabled = true;
@@ -411,15 +266,6 @@ public class LobbyGUI : MonoBehaviour {
     void OnConnectedToServer() {
         Network.SetSendingEnabled(0, false);
         Network.isMessageQueueRunning = false;
-        LoadArenaLevel();
-    }
-
-    void OnServerInitialized() {
-        LoadArenaLevel();
-        Debug.Log("Server initialized and ready");
-    }
-
-    void LoadArenaLevel() {
         Network.SetLevelPrefix(GameConstants.LEVEL_PREFIX_ARENA);
         Application.LoadLevel(GameConstants.LEVEL_ARENA);
     }
--- combo-combat-unity/Assets/Scripts/GUI/SplashGUI.cs
+++ combo-combat-unity/Assets/Scripts/GUI/SplashGUI.cs
@@ -5,6 +5,16 @@ using Boomlagoon.JSON;
 
 public class SplashGUI : MonoBehaviour {
 
+    public string gameName = "You must change this";
+    public bool allowsDedicatedServer = true;
+
+    private ConnectionTesterStatus connectionTestResult = ConnectionTesterStatus.Undetermined;
+    private string testMessage = "Undetermined NAT capabilities";
+    private bool probingPublicIP = false;
+    private bool doneTesting = false;
+    private double timer = 0.0;
+    private bool useNat = true;
+
     public Texture logo;
     public string DEFAULT_USERNAME = "Player";
     public int maxUsernameLength = 25;
@@ -14,7 +24,13 @@ public class SplashGUI : MonoBehaviour {
     private string password;
     private string message;
 
+    void Update() {
+        // If test is undetermined, keep running
+        if (!doneTesting)
+            TestConnection();
+    }
     void Awake() {
+        connectionTestResult = Network.TestConnection();
 
         password = "";
         username = PlayerPrefs.GetString(GameConstants.PREFS_USERNAME);
@@ -23,10 +39,27 @@ public class SplashGUI : MonoBehaviour {
         }
 
         if (CommandLineParser.IsBatchMode()) {
-            Debug.Log("salteando ");
-            GoToLobby();
+            DoStartServer();
         }
+    }
 
+    void DoStartServer() {
+        NetworkConnectionError error = NetworkConnectionError.CreateSocketOrThreadFailure;
+        int port = CommandLineParser.GetServerPort();
+        while (error != NetworkConnectionError.NoError) {
+            error = Network.InitializeServer(CommandLineParser.GetMaxPlayersAllowed(), port, useNat);
+            if (CommandLineParser.IsDynamicPort()) {
+                port += 1;
+            } else {
+                if (error != NetworkConnectionError.NoError)
+                    Debug.LogError(error);
+                break;
+            }
+            if (port > CommandLineParser.GetServerPort() + 100) {
+                break; // timeout
+            }
+        }
+        MasterServer.RegisterHost(gameName, CommandLineParser.GetServerName(), CommandLineParser.GetServerComment());
     }
 
     void OnGUI() {
@@ -37,7 +70,11 @@ public class SplashGUI : MonoBehaviour {
         int formHeight = 200;
 
         GUI.Label(new Rect(Screen.width / 2 - logoSize / 2, Screen.height / 2 - logoSize * 2 / 3, logoSize, logoSize), logo);
-
+        if (allowsDedicatedServer) {
+            if (GUILayout.Button("Start Server")) {
+                DoStartServer();
+            }
+        }
         GUILayout.BeginArea(new Rect((sWidth - formWidth) / 2, (sHeight - formHeight), formWidth, formHeight));
         GUILayout.BeginHorizontal();
         GUILayout.Button("Username: ");
@@ -108,6 +145,86 @@ public class SplashGUI : MonoBehaviour {
     private void GoToLobby() {
         Network.SetLevelPrefix(GameConstants.LEVEL_PREFIX_LOBBY);
         Application.LoadLevel(GameConstants.LEVEL_LOBBY);
+    }
+
+    void TestConnection() {
+        // Start/Poll the connection test, report the results in a label and react to the results accordingly
+        connectionTestResult = Network.TestConnection();
+        switch (connectionTestResult) {
+            case ConnectionTesterStatus.Error:
+                testMessage = "Problem determining NAT capabilities";
+                doneTesting = true;
+                break;
+
+            case ConnectionTesterStatus.Undetermined:
+                testMessage = "Undetermined NAT capabilities";
+                doneTesting = false;
+                break;
+
+            case ConnectionTesterStatus.PublicIPIsConnectable:
+                testMessage = "Directly connectable public IP address.";
+                useNat = false;
+                doneTesting = true;
+                break;
+
+            // This case is a bit special as we now need to check if we can 
+            // circumvent the blocking by using NAT punchthrough
+            case ConnectionTesterStatus.PublicIPPortBlocked:
+                testMessage = "Non-connectble public IP address (port " + CommandLineParser.GetServerPort() + " blocked), running a server is impossible.";
+                useNat = false;
+                // If no NAT punchthrough test has been performed on this public IP, force a test
+                if (!probingPublicIP) {
+                    connectionTestResult = Network.TestConnectionNAT();
+                    probingPublicIP = true;
+                    timer = Time.time + 10;
+                }
+                    // NAT punchthrough test was performed but we still get blocked
+                else if (Time.time > timer) {
+                    probingPublicIP = false; 		// reset
+                    useNat = true;
+                    doneTesting = true;
+                }
+                break;
+            case ConnectionTesterStatus.PublicIPNoServerStarted:
+                testMessage = "Public IP address but server not initialized, it must be started to check server accessibility. Restart connection test when ready.";
+                break;
+
+            case ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted:
+                testMessage = "Limited NAT punchthrough capabilities. Cannot connect to all types of NAT servers.";
+                useNat = true;
+                doneTesting = true;
+                break;
+
+            case ConnectionTesterStatus.LimitedNATPunchthroughSymmetric:
+                testMessage = "Limited NAT punchthrough capabilities. Cannot connect to all types of NAT servers. Running a server is ill adviced as not everyone can connect.";
+                useNat = true;
+                doneTesting = true;
+                break;
+
+            case ConnectionTesterStatus.NATpunchthroughAddressRestrictedCone:
+            case ConnectionTesterStatus.NATpunchthroughFullCone:
+                testMessage = "NAT punchthrough capable. Can connect to all servers and receive connections from all clients. Enabling NAT punchthrough functionality.";
+                useNat = true;
+                doneTesting = true;
+                break;
+
+            default:
+                testMessage = "Error in test routine, got " + connectionTestResult;
+                break;
+        }
+        if (!testMessage.Contains("Undetermined")) {
+            //Debug.Log(testMessage);
+        }
+    }
+
+    void OnServerInitialized() {
+        Network.SetLevelPrefix(GameConstants.LEVEL_PREFIX_ARENA);
+        Application.LoadLevel(GameConstants.LEVEL_ARENA);
+        Debug.Log("Server initialized and ready");
+    }
+
+    void OnFailedToConnectToMasterServer(NetworkConnectionError info) {
+        Debug.Log(info);
     }
 
 }
